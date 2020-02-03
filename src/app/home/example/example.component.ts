49f3a4082bf72078c6864a95031212c552a5cc35
Minchul Chae <mcchae7@gmail.com>
2020-02-02 21:49:39 -0800

    add css specificity

--- src/app/home/example/example.component.ts
+++ src/app/home/example/example.component.ts
@@ -37,7 +37,12 @@ export class ExampleComponent extends AppBaseComponent {
     {
       id: '../../../assets/mp3/EventLoopAndWebWorkers.mp3',
       name: 'Javascript Event loop and Web Workers',
-      date: '02/26/2020'
+      date: '01/26/2020'
+    },
+    {
+      id: '../../../assets/mp3/CSS_Specificity.mp3',
+      name: 'CSS Specificity',
+      date: '02/02/2020'
     }];
 
   constructor(
